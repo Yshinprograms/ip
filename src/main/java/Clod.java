855bab366c410ffb45d6e586e9d9c55e4ff36757
Yshinprograms <82699007+Yshinprograms@users.noreply.github.com>
2025-01-21 16:04:06 +0800

    Add new name & greeting

---
+++ src/main/java/Clod.java
@@ -0,0 +1,15 @@
+public class Clod {
+    public static void main(String[] args) {
+        String logo = "┏┓┓ ┏┓┳┓\n"
+                + "┃ ┃ ┃┃┃┃\n"
+                + "┗┛┗┛┗┛┻┛\n";
+        System.out.println("Welcome to\n" + logo);
+
+        // Clod as a play on Claude with a Confused Purpose & Identity
+        // Greeting message
+        System.out.println("Hi, I'm Clod. Am I a helper bot? Or a... what was that word... a... thingy? I don't know.");
+        // Exit message
+        System.out.println("Okay... I'm gone now. ...Was that it? ...Hmm. Bye.");
+    }
+}
+
