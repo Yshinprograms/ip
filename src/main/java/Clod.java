3b4493d12901da2d9271b9f154739bbbd0db7603
Yshinprograms <82699007+Yshinprograms@users.noreply.github.com>
2025-01-27 12:09:00 +0800

    Add ability to store user inputs and display when requested

--- src/main/java/Clod.java
+++ src/main/java/Clod.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 // Clod as a play on Claude with a Confused Purpose & Identity
 public class Clod {
@@ -10,13 +11,35 @@ public class Clod {
         printStartMessage();
 
         String line = takeInput();
-
+        ArrayList<String> userInputList = new ArrayList<String>();
         while (!containsBye(line)){
-            echoUserInput(line);
+            //echoUserInput(line);
+            if (line.toLowerCase().contains("list")){
+                printList(userInputList);
+            }
+            else {
+                addUserInputToList(line, userInputList);
+            }
             line = takeInput();
         }
 
         printExitMessage();
+    }
+
+    public static void printList(ArrayList<String> userInputList){
+        System.out.println("Here's what you've said so far:");
+        int count = 1;
+        for (String addedLine : userInputList){
+            System.out.print(count + ". ");
+            echoUserInput(addedLine);
+            count++;
+        }
+    }
+
+    public static void addUserInputToList(String line, ArrayList userInputList){
+        userInputList.add(line);
+        System.out.print("Added to list: ");
+        echoUserInput(line);
     }
 
     public static boolean containsBye(String line){
@@ -24,21 +47,25 @@ public class Clod {
     }
 
     public static String takeInput(){
+        // Start on the next line
+        System.out.println();
         Scanner in = new Scanner(System.in);
+        // Clod prompts user for input
         printUserPrefix(false);
         System.out.println("What's that? Did you say something?");
+        //System.out.println();
+        // User input
         printUserPrefix(true);
         return in.nextLine();
+    }
+
+    public static void echoUserInput(String line){
+        System.out.println(line);
     }
 
     public static void printStartMessage(){
         printUserPrefix(false);
         System.out.println("Hi, I'm Clod. Am I a helper bot? Or a... what was that word... a... thingy? I don't know.");
-    }
-
-    public static void echoUserInput(String line){
-        printUserPrefix(false);
-        System.out.println(line);
     }
 
     public static void printExitMessage(){
